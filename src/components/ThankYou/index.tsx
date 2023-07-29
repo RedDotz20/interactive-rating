dd81e5e022c5f3b94cac0af87b13e770e54e3b5a
RedDotz20 <carlos_tabangay@outlook.com>
2023-07-29 23:55:05 +0800

    feat: thankYou component

--- src/components/ThankYou/index.tsx
+++ src/components/ThankYou/index.tsx
@@ -1,12 +1,23 @@
 import { useLocation } from 'react-router-dom';
+import illustration from '../../assets/illustration-thank-you.svg';
+import './thankyou.module.css';
 
 export default function ThankYou() {
-	const location = useLocation();
+	const location = useLocation(),
+		ratingSelected = location.state.rating;
 
 	return (
-		<div>
-			<h1>Thank You Page</h1>
-			<p>{location.state.rating}</p>
-		</div>
+		<section>
+			<img
+				src={illustration}
+				alt="Thank You"
+			/>
+			<h2>You selected {ratingSelected} out of 5</h2>
+			<h1>Thank you!</h1>
+			<p>
+				We appreciate you taking the time to give a rating. if you ever need
+				more support, don't hesitate to get in touch!
+			</p>
+		</section>
 	);
 }
